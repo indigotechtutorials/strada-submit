081df20126a8c379c93304fbd28bc4ab982fe85d
Indigo Tech <yungboiiindigo@gmail.com>
2024-05-16 12:54:33 -0500

    initial commit

--- strada-submit/BridgeComponent+App.swift
+++ strada-submit/BridgeComponent+App.swift
@@ -4,5 +4,13 @@
 //
 //  Created by Indigo Youtube on 5/16/24.
 //
-
 import Foundation
+import Strada
+
+extension BridgeComponent {
+    nonisolated static var allTypes: [BridgeComponent.Type] {
+        [
+            FormComponent.self,
+        ]
+    }
+}
--- strada-submit/WebViewController.swift
+++ strada-submit/WebViewController.swift
@@ -5,4 +5,54 @@
 //  Created by Indigo Youtube on 5/16/24.
 //
 
-import Foundation
+import Turbo
+import Strada
+import WebKit
+
+class WebViewController: VisitableViewController, BridgeDestination {
+    private lazy var bridgeDelegate: BridgeDelegate = {
+            BridgeDelegate(location: visitableURL.absoluteString,
+                           destination: self,
+                           componentTypes: BridgeComponent.allTypes)
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bridgeDelegate.onViewDidLoad()
+    }
+    
+    
+    override func visitableDidRender() {
+        title = "Strada Submit"
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+       super.viewWillAppear(animated)
+       bridgeDelegate.onViewWillAppear()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+       super.viewDidAppear(animated)
+       bridgeDelegate.onViewDidAppear()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+       super.viewWillDisappear(animated)
+       bridgeDelegate.onViewWillDisappear()
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+       super.viewDidDisappear(animated)
+       bridgeDelegate.onViewDidDisappear()
+    }
+
+    // MARK: Visitable
+
+    override func visitableDidActivateWebView(_ webView: WKWebView) {
+       bridgeDelegate.webViewDidBecomeActive(webView)
+    }
+
+    override func visitableDidDeactivateWebView() {
+       bridgeDelegate.webViewDidBecomeDeactivated()
+    }
+}
