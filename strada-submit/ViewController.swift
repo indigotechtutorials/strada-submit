3b56bc3bce9f59cb33c2b5015b06bbe65f754c03
Indigo Tech <yungboiiindigo@gmail.com>
2024-05-16 07:24:35 -0500

    Initial Commit

---
+++ strada-submit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  strada-submit
+//
+//  Created by Indigo Youtube on 5/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
